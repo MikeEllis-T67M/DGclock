f831e51848accdef9d8c755f22159a7eb8a00972
Mike Ellis <git@mikeellis.org.uk>
2020-08-02 21:46:34 +0100

    Testing driving the motor

--- src/main.py
+++ src/main.py
@@ -65,3 +65,39 @@ tft.text(120 - int(tft.textWidth(text)/2), 60 - int(tft.fontSize()[1]/2), text, 
 #t = TouchPad(Pin(2))
 #while True:
 #    print("Touch read: {}".format(t.read()))
+
+# Next thing - get the motor driver working to step the hands
+motor_en = machine.Pin(36, Pin.OUT)
+motor_A  = machine.Pin(37, Pin.OUT)
+motor_B  = machine.Pin(38, Pin.OUT)
+
+motor_en.off()
+motor_A.off()
+motor_B.off()
+
+import utime
+
+# Let's start off with 200ms pulses once per second
+pulse_duration = 200
+pulse_rate     = 1000
+
+while True:
+    # Pulse polarity positive
+    motor_A.on()
+    motor_en.on()
+    utime.sleep_ms(pulse_duration)
+    motor_en.off() # Keep the driver disabled as much as possible to save power/heat
+    motor_A.off()
+
+    # Wait
+    utime.sleep_ms(pulse_rate - pulse_duration)
+
+    # Pulse polarity negative
+    motor_B.on()
+    motor_en.on()
+    utime.sleep_ms(pulse_duration)
+    motor_en.off() # Keep the driver disabled as much as possible to save power/heat
+    motor_B.off()
+
+    utime.sleep_ms(pulse_rate - pulse_duration
+
