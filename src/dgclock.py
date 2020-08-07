bb0f9cf4e3870f798246acd42f21bc314a0327cb
Mike Ellis <mike.ellis@bbc.co.uk>
2020-08-07 15:10:29 +0100

    Made closedown a little more useful

--- src/dgclock.py
+++ src/dgclock.py
@@ -12,26 +12,28 @@ invert        = hand_position[5] % 2 == 1 # Is the second hand pointing to an ev
 display       = hand_position[3] * 3600 + hand_position[4] * 60 + hand_position[5]  % 43200
 
 pc = pulseclock.PulseClock(Pin(26, Pin.OUT), Pin(25, Pin.OUT), Pin(27, Pin.OUT), 200, 200, invert)
-
-while True:
-    current_time = ds.rtc_tm
-
-    # Convert both to seconds since midnight
-    current = (current_time[3]  * 3600 + current_time[4]  * 60 + current_time[5]) % 43200 # Convert to 12-hour format
-
-    diff    = current - display # How far apart are the hands - allowing for wrap-around
-
-    if diff > 0 or diff < -3600: # If the difference is around minus one hour, it's quicker just to stop the clock
-        # Update the stored hand position and step the clock
-        display = (display + 1) % 43200
-        new_hand_position = (0, 0, 0, (display / 3600), (display / 60) % 60, display % 60, 0, 0) 
-
-        # Update the stored hand position
-        ds.alarm1_tm = new_hand_position
-
-        # Move the clock - note that there is a potential race condition here
-        pc.step()
-        print("RTC:{} Old:{} Diff:{}  New:{}".format(current, display, diff, new_hand_position))
-    else:
-        sleep_ms(100)
-
+try:
+    while True:
+        current_time = ds.rtc_tm
+    
+        # Convert both to seconds since midnight
+        current = (current_time[3]  * 3600 + current_time[4]  * 60 + current_time[5]) % 43200 # Convert to 12-hour format
+    
+        diff    = current - display # How far apart are the hands - allowing for wrap-around
+    
+        if diff > 0 or diff < -3600: # If the difference is around minus one hour, it's quicker just to stop the clock
+            # Update the stored hand position and step the clock
+            display = (display + 1) % 43200
+            new_hand_position = (0, 0, 0, int(display / 3600), int(display / 60) % 60, display % 60, 0, 0) 
+    
+            # Update the stored hand position
+            ds.alarm1_tm = new_hand_position
+    
+            # Move the clock - note that there is a potential race condition here
+            pc.step()
+            print("RTC:{} Old:{} Diff:{}  New:{}".format(current, display, diff, new_hand_position))
+        else:
+            sleep_ms(100)
+except:
+    ds.rtc_tm
+    ds.alarm1_tm
