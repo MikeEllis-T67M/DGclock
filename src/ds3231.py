8890b17d5dd38d4edb242db2a25c2f364628d57c
Mike Ellis <mike.ellis@bbc.co.uk>
2020-08-07 11:07:11 +0100

    Added support for Alarm1

--- src/ds3231.py
+++ src/ds3231.py
@@ -24,7 +24,7 @@ class DS3231:
         if DS3231_I2C_ADDR not in self.ds3231.scan():
             raise RuntimeError("DS3231 not found on I2C bus at %d" % DS3231_I2C_ADDR)
 
-    def __repl__(self):
+    def __repr__(self):
         '''Returns representation of the object'''
         return("{}({!r})".format(self.__class__.__name__, self.ds3231))
 
@@ -54,16 +54,16 @@ class DS3231:
 
     @staticmethod
     def tm2dsrtc(tm):
-        """ Convert human-normal time and date into DS format
+        """ Convert tm format tuple into DS3231 RTC register format
         Args:
-            tm (int): The current year - 1900...2099 (or 00...99 for 2000...2099)
+            tm (tuple): The alarm time in TM format - can specify day of week or date, not both. Will default to date if both are set.
         Returns:
             bytearray in DS time format
         """        
         ds_format = bytearray((DS3231.dec2bcd(tm[5]),        # Seconds
                                DS3231.dec2bcd(tm[4]),        # Minutes
                                DS3231.dec2bcd(tm[3]),        # Hours
-                               DS3231.dec2bcd(tm[6]) + 1,    # Day of week
+                               DS3231.dec2bcd(tm[6] + 1),    # Day of week - TM has days 0-6, DS has 1-7
                                DS3231.dec2bcd(tm[2]),        # Day of month
                                DS3231.dec2bcd(tm[1]),        # Month
                                DS3231.dec2bcd(tm[0] % 100))) # Only the year within the century
@@ -72,8 +72,8 @@ class DS3231:
         return ds_format
 
     @staticmethod
-    def dsrtc2hms(ds_format):
-        """ Convert DS-format time/date to mktime-compatible time and date
+    def dsrtc2tm(ds_format):
+        """ Convert DS3231 register format into tm-format
         Args:
             ds_format : bytearray(7) containing the DS format data
 
@@ -88,11 +88,61 @@ class DS3231:
         month  = DS3231.bcd2dec(ds_format[5] & 0x1f) # Filter off the century bit
         year   = DS3231.bcd2dec(ds_format[6]) + 1900 # Assume 1900-1999
 
-        if (ds_format[2] & 0x60) != 0x60:
+        if (ds_format[2] & 0x60) == 0x60:
             hour -= 8 # In 12 hour mode, and PM set, but BCD conversion will have +20, so -8
 
         if (ds_format[5] & 0x80) != 0:
             year += 100 # Update the year if the century bit is set
+
+        return (year, month, day, hour, minute, second, day-1, 0) # Build localtime format
+
+    @staticmethod
+    def tm2dsal1(tm):
+        """ Convert tm format tuple into DS3231 RTC register format
+        Args:
+            tm (tuple): The alarm time in TM format - can specify day of week or date, not both. Will default to date if both are set.
+        Returns:
+            bytearray in DS alarm1 format
+        """        
+        if tm[2] = 0:
+            # Day of week mode since date is outside the valid range (1-31)
+            ds_format = bytearray((DS3231.dec2bcd(tm[5]),               # Seconds
+                                   DS3231.dec2bcd(tm[4]),               # Minutes
+                                   DS3231.dec2bcd(tm[3]),               # Hours
+                                   DS3231.dec2bcd(tm[6] + 1) + 0x40)    # Day of week
+        else:
+            # Date mode since date is outside the valid range (1-31)
+            ds_format = bytearray((DS3231.dec2bcd(tm[5]),               # Seconds
+                                   DS3231.dec2bcd(tm[4]),               # Minutes
+                                   DS3231.dec2bcd(tm[3]),               # Hours
+                                   DS3231.dec2bcd(tm[2]))               # Date
+        return ds_format
+
+    @staticmethod
+    def dsal12tm(ds_format):
+        """ Convert DS3231 register format into tm-format
+        Args:
+            ds_format : bytearray(4) containing the DS format data
+
+        Returns:
+            A mktime-compatible tuple. Day of week is as in the DS format message, and day of year is zero
+        """        
+        second = DS3231.bcd2dec(ds_format[0])
+        minute = DS3231.bcd2dec(ds_format[1])
+        hour   = DS3231.bcd2dec(ds_format[2] & 0x3f) # Filter off the 12/24 bit
+        if (ds_format[2] & 0x60) == 0x60:
+            hour -= 8 # In 12 hour mode, and PM set, but BCD conversion will have +20, so -8
+
+        if ds_format[3] & 0x40: 
+            # Alarm in "day of week" mode
+            day    = DS3231.bcd2dec(ds_format[3] & 0x0f) - 1 # DS range 1-7, TM range 0-6
+            date   = 0                                       # TM valid range is 1-31
+        else:
+            day    = 0                                       # Strictly speaking not correct
+            date   = DS3231.bcd2dec(ds_format[3] & 0x3f)
+
+        month  = 0
+        year   = 0
 
         return (year, month, day, hour, minute, second, day-1, 0) # Build localtime format
 
@@ -108,11 +158,23 @@ class DS3231:
         self.ds3231.readfrom_mem_into(DS3231_I2C_ADDR, 0, buffer)
         return buffer
 
+    def read_ds3231_alarm1(self):
+        """ Read the Alarm1 from the DS3231
+
+        Args:
+
+        Returns:
+            A DS3231 formatted bytearray for addresses 7-10 inclusive
+        """    
+        buffer = bytearray(4)
+        self.ds3231.readfrom_mem_into(DS3231_I2C_ADDR, 7, buffer)
+        return buffer
+
     @property
-    def localtime(self):
-        """ Read the DS3231 RTC and return the time as a localtime
+    def rtc_tm(self):
+        """ Read the DS3231 RTC and return the time as a tm tuple
         """
-        return DS3231.dsrtc2hms(self.read_ds3231_rtc())
+        return DS3231.dsrtc2tm(self.read_ds3231_rtc())
 
     @property
     def rtc(self):
@@ -122,6 +184,38 @@ class DS3231:
 
     @rtc.setter
     def rtc(self, time_to_set):
-        """ Set the DS3231 RTC to the time since epoch given
+        """ Set the DS3231 RTC
+
+        Args:
+            time_to_set (number): Seconds since epoch
         """
         self.ds3231.writeto_mem(DS3231_I2C_ADDR, 0, DS3231.tm2dsrtc(utime.localtime(time_to_set)))
+
+    @property
+    def alarm1_tm(self):
+        """ Read the DS3231 RTC and return the time as a tm tuple
+
+        Returns:
+            tuple: Standard tm date tuple
+            
+        Note:
+            AL1 only has Day/Date:HH:MM:SS fields. YY and MON will therefore always be zero.
+            If the DS AL1 is in Date mode, then Date will be in the range 1-31 and Day will be 0.
+            if the DS AL1 is in Day of Week mode, then Date will be 0 Day will be in the range 0-6.
+        
+        """
+        return DS3231.dsal12tm(self.read_ds3231_alarm1())
+
+    @alarm1_tm.setter
+    def alarm1_tm(self, time_to_set):
+        """ Set the DS3231 RTC to the tm tuple given
+
+        Args:
+            time_to_set (tuple): The standard tm tuple description of the desired alarm time. 
+        
+        Notes:
+            AL1 only uses Day/Date:HH:MM:SS fields. 
+            If Date=0 then Day mode is used, otherwise the date mode is used.
+        
+        """
+        self.ds3231.writeto_mem(DS3231_I2C_ADDR, 7, DS3231.tm2dsal1(time_to_set))
