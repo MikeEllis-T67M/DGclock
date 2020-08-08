b0aee7e6b734aae66590b347b34490f06026fbec
Mike Ellis <mike.ellis@bbc.co.uk>
2020-08-08 11:43:28 +0100

    A bit of tidying

--- src/dgclock.py
+++ src/dgclock.py
@@ -15,35 +15,30 @@ hand_position = ds.alarm1_tm
 invert        = hand_position[5] % 2 == 1 # Is the second hand pointing to an even or odd number?
 display       = hand_position[3] * 3600 + hand_position[4] * 60 + hand_position[5]  % 43200
 
+# Initialise the pulse clock itself
 pc = pulseclock.PulseClock(Pin(26, Pin.OUT), Pin(25, Pin.OUT), Pin(27, Pin.OUT), 150, 100, invert)
-try:
-    while True:
-        current_time = rtc.now()
-    
-        # Convert both to seconds since midnight
-        current = (current_time[3]  * 3600 + current_time[4]  * 60 + current_time[5]) % 43200 # Convert to 12-hour format
-    
-        diff    = current - display # How far apart are the hands - allowing for wrap-around
-    
-        if diff > 0 or diff < -7200: # If the difference is less than two hours, it's quicker just to stop the clock
-            # Update the stored hand position and step the clock
-            display = (display + 1) % 43200
-            new_hand_position = (0, 0, 0, (display // 3600), (display // 60) % 60, display % 60, 0, 0) 
-    
-            # Update the stored hand position
-            ds.alarm1_tm = new_hand_position
-    
-            # Move the clock - note that there is a potential race condition here
-            pc.step()
-        else:
-            sleep_ms(100)
 
-        # Re-sync the clocks every 15 minutes at HH:01:02, HH:16:02, HH:31:02 and HH:46:02
-        if (current_time % 900) == 62:
-            if rtc.synced():
-                ds_rtc_tm = rtc.now() # Copy from RTC to DS if the RTC is NTP synced
-            else:
-                rtc.init(ds.rtc_tm) # Otherwise copy from the DS to the RTC
-except:
-    print(ds.rtc_tm)
-    print(ds.alarm1_tm)
+while True:
+    # Convert current time to seconds since 00:00:00 (12-hour clock mode)
+    current_time = rtc.now()
+    current = (current_time[3]  * 3600 + current_time[4]  * 60 + current_time[5]) % 43200
+
+    # How far apart are the hands - allowing for wrap-around
+    diff = current - display 
+    if diff > 0 or diff < -7200: # If the difference is less than two hours, it's quicker just to stop the clock
+        # Update the stored hand position
+        display           = (display + 1) % 43200
+        new_hand_position = (0, 0, 0, (display // 3600), (display // 60) % 60, display % 60, 0, 0) 
+        ds.alarm1_tm      = new_hand_position
+
+        # Move the clock - note that there is a potential race condition here
+        pc.step()
+    else:
+        sleep_ms(100)
+        
+    # Re-sync the clocks every 15 minutes at HH:01:02, HH:16:02, HH:31:02 and HH:46:02
+    if (current_time % 900) == 62:
+        if rtc.synced():
+            ds_rtc_tm = rtc.now() # Copy from RTC to DS if the RTC is NTP synced
+        else:
+            rtc.init(ds.rtc_tm) # Otherwise copy from the DS to the RTC
