096f766b068d241434cf7d9784d3a53028b53fe9
Mike Ellis <mike.ellis@bbc.co.uk>
2020-08-11 08:46:53 +0100

    Slightly better tidying up, including the display

--- src/dgclock.py
+++ src/dgclock.py
@@ -96,5 +96,7 @@ def dgclock():
 
     except KeyboardInterrupt:
         # Try to relinquish the I2C bus
+        print("Hand position: {}", format(ds.alarm1_tm))
         i2c.deinit()
+        tft.deinit()
         # del i2c
