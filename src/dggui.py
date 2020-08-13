2b2f09db10dfa0c06a8d34e7ded855108b53bacb
Mike Ellis <mike.ellis@bbc.co.uk>
2020-08-13 13:41:56 +0100

    Added web server to the dggui

--- src/dggui.py
+++ src/dggui.py
@@ -6,6 +6,114 @@ import display
 import settings
 import wifi
 
+from microWebSrv import MicroWebSrv
+
+# ----------------------------------------------------------------------------
+
+@MicroWebSrv.route('/test')
+def _httpHandlerTestGet(httpClient, httpResponse) :
+	content = """\
+	<!DOCTYPE html>
+	<html lang=en>
+        <head>
+        	<meta charset="UTF-8" />
+            <title>TEST GET</title>
+        </head>
+        <body>
+            <h1>TEST GET</h1>
+            Client IP address = %s
+            <br />
+			<form action="/test" method="post" accept-charset="ISO-8859-1">
+				First name: <input type="text" name="firstname"><br />
+				Last name: <input type="text" name="lastname"><br />
+				<input type="submit" value="Submit">
+			</form>
+        </body>
+    </html>
+	""" % httpClient.GetIPAddr()
+	httpResponse.WriteResponseOk( headers		 = None,
+								  contentType	 = "text/html",
+								  contentCharset = "UTF-8",
+								  content 		 = content )
+
+
+@MicroWebSrv.route('/test', 'POST')
+def _httpHandlerTestPost(httpClient, httpResponse) :
+	formData  = httpClient.ReadRequestPostedFormData()
+	firstname = formData["firstname"]
+	lastname  = formData["lastname"]
+	content   = """\
+	<!DOCTYPE html>
+	<html lang=en>
+		<head>
+			<meta charset="UTF-8" />
+            <title>TEST POST</title>
+        </head>
+        <body>
+            <h1>TEST POST</h1>
+            Firstname = %s<br />
+            Lastname = %s<br />
+        </body>
+    </html>
+	""" % ( MicroWebSrv.HTMLEscape(firstname),
+		    MicroWebSrv.HTMLEscape(lastname) )
+	httpResponse.WriteResponseOk( headers		 = None,
+								  contentType	 = "text/html",
+								  contentCharset = "UTF-8",
+								  content 		 = content )
+
+
+@MicroWebSrv.route('/edit/<index>')             # <IP>/edit/123           ->   args['index']=123
+@MicroWebSrv.route('/edit/<index>/abc/<foo>')   # <IP>/edit/123/abc/bar   ->   args['index']=123  args['foo']='bar'
+@MicroWebSrv.route('/edit')                     # <IP>/edit               ->   args={}
+def _httpHandlerEditWithArgs(httpClient, httpResponse, args={}) :
+	content = """\
+	<!DOCTYPE html>
+	<html lang=en>
+        <head>
+        	<meta charset="UTF-8" />
+            <title>TEST EDIT</title>
+        </head>
+        <body>
+	"""
+	content += "<h1>EDIT item with {} variable arguments</h1>"\
+		.format(len(args))
+	
+	if 'index' in args :
+		content += "<p>index = {}</p>".format(args['index'])
+	
+	if 'foo' in args :
+		content += "<p>foo = {}</p>".format(args['foo'])
+	
+	content += """
+        </body>
+    </html>
+	"""
+	httpResponse.WriteResponseOk( headers		 = None,
+								  contentType	 = "text/html",
+								  contentCharset = "UTF-8",
+								  content 		 = content )
+
+# ----------------------------------------------------------------------------
+
+def _acceptWebSocketCallback(webSocket, httpClient) :
+	print("WS ACCEPT")
+	webSocket.RecvTextCallback   = _recvTextCallback
+	webSocket.RecvBinaryCallback = _recvBinaryCallback
+	webSocket.ClosedCallback 	 = _closedCallback
+
+def _recvTextCallback(webSocket, msg) :
+	print("WS RECV TEXT : %s" % msg)
+	webSocket.SendText("Reply for %s" % msg)
+
+def _recvBinaryCallback(webSocket, data) :
+	print("WS RECV DATA : %s" % data)
+
+def _closedCallback(webSocket) :
+	print("WS CLOSED")
+
+# ----------------------------------------------------------------------------
+
 def text_centred(tft, text, vpos):
     """ Display some text centred on the screen
 
@@ -16,7 +124,7 @@ def text_centred(tft, text, vpos):
     """    
     tft.text(120 - int(tft.textWidth(text)/2), vpos - int(tft.fontSize()[1]/2), text)
 
-def dgclock():
+def dggui():
     # Initialise the display
     tft = display.TFT() 
     tft.init(tft.ST7789,bgr=False,rot=tft.LANDSCAPE, miso=17,backl_pin=4,backl_on=1, mosi=19, clk=18, cs=5, dc=16,splash = False)
@@ -34,11 +142,25 @@ def dgclock():
     print('AP config: {}'.format(ap_info))
     text_centred(tft, "{}".format(ap_info[0]), 60)
 
-    # Initialised the FreeRTOS RTC from the DS3231 battery-backed RTC, and set up NTP sync every 15 minutes
+    # Initialise the FreeRTOS RTC and set up NTP sync every 15 minutes
     rtc = RTC()
     print("RTC set to    : {}".format(rtc.now()))
     rtc.ntp_sync("pool.ntp.org", update_period = 900)
     recent_sync = False
+
+    # ----------------------------------------------------------------------------
+
+    #routeHandlers = [
+    #	( "/test",	"GET",	_httpHandlerTestGet ),
+    #	( "/test",	"POST",	_httpHandlerTestPost )
+    #]
+
+    # Initialise the demo website
+    srv = MicroWebSrv(webPath='www/')
+    srv.MaxWebSocketRecvLen     = 256
+    srv.WebSocketThreaded		= False
+    srv.AcceptWebSocketCallback = _acceptWebSocketCallback
+    srv.Start()
 
     try:
         while True:
