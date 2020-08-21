c21ce7049501b4b856b358b273cf4dbaf40e9684
Mike Ellis <mike.ellis@bbc.co.uk>
2020-08-21 15:59:47 +0100

    Fixed Catchup/Wait bug

--- src/dgclock.py
+++ src/dgclock.py
@@ -19,7 +19,7 @@ class DGClock:
 
         # Initialise the actual pulse clock
         self.pc   = pulseclock.PulseClock(clock_settings, hands % 2 == 0)
-        self.mode = "Stopped"
+        self.mode = "Wait"
 
     def __repr__(self):
         pass
@@ -49,17 +49,17 @@ class DGClock:
     def move(self, wanted_time):
         wanted_time %= 43200 # Only care about the 12-hour portion of the time
 
-        diff = self._hands - wanted_time
+        diff = (wanted_time - self._hands) % 43200
 
-        if diff == 0:                          # Hands are correct
-            self.mode = "Running"
-        elif diff == 1:                        # Just need a single step
-            self.mode = "Running"
+        if diff == 0:                                 # Hands are correct
+            self.mode = "Run"
+        elif diff == 1:                               # Just need a single step
+            self.mode = "Run"
             self._incr_hands()
             self.pc.step()
-        elif diff < 3600 and hands % 60 == 0:  # <2hr difference and second hand on 12
-            self.mode = "Stopped"
-        else:                                  # Need to move fast to catch up
-            self.mode = "Catch-up"
+        elif diff > 36000 and self._hands % 60 == 0:  # >10hr difference and second hand on 12
+            self.mode = "Wait"
+        else:                                         # Need to move fast to catch up
+            self.mode = "Fast"
             self._incr_hands()
             self.pc.faststep()
