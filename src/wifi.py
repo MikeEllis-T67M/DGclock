fa3fec31e1a04df97f20a846a8c927154cd55b61
Mike Ellis <git@mikeellis.org.uk>
2020-12-28 10:16:08 +0000

    Added support for multi-WiFi and auto-reconnect

--- src/wifi.py
+++ src/wifi.py
@@ -1,6 +1,56 @@
 import network
 from utime import sleep_ms
 
+class wifi:
+    def __init__(self, config):
+        self.config     = config
+        self.connection = 0
+        self.tries      = 0
+        self.sta        = network.WLAN(network.STA_IF)
+        self.connected  = False
+
+
+    def __repr__(self):
+        '''Returns representation of the object'''
+        return("{}({!r})".format(self.__class__.__name__, self.config))
+
+    def connect(self):
+        """ Ensure we have a network connection. If not connection, try each set of credentials in turn
+            up to ten times before trying the next connection.
+        """        
+        if self.sta.isconnected():  # If we have an active connection, our work is done.
+            if not self.connected:
+                print("Connected to connection {}".format(self.connection))
+                self.connected = True
+            return
+
+        self.connected = False
+
+        if self.tries > 10: # Tried this connect ten times - try the next one
+            self.tries       = 0
+            self.connection += 1
+            self.connection %= len(self.config)
+
+        # We're not connected - so try again
+        print("Trying WiFi connection #{}".format(self.connection))
+        self.sta.active(True)
+        sleep_ms(20)
+        self.sta.config(dhcp_hostname = self.config[self.connection]['Hostname'])
+        self.sta.connect(self.config[self.connection]['SSID'], self.config[self.connection]['Password'])
+        self.tries += 1
+
+    def get_ip_addr(self):
+        """ Get the current IP address
+
+        Returns:
+            String: Current IP address, or None if not connected
+        """        
+        if self.sta.isconnected():
+            return self.sta.ifconfig()[0]
+        else:
+            return None          
+
+# ---- Old code below here -----------------------------------------------------------------------------------
 def connect_sta(ssid, password, hostname):
     """ Connect to a WiFI Access Point and obtain a DHCP address
 
